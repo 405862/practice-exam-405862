043e0bdc62540716a1d8afb822b3c49c417155fa
405862_BARRIONUEVO_SANTIAGO <405862@tecnicatura.frc.utn.edu.ar>
2024-11-07 23:58:06 -0300

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CreateOrderComponent } from './create-order/create-order.component';
+import { OrderlistComponent } from './orderlist/orderlist.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
